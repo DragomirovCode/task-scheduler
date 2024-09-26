4691c814e22711e348b1ee7a7e234510a5ac2db1
mesha <mikhail_dragomirov@mail.ru>
2024-09-26 12:42:27 +0300

    feat(002): обновил код

--- src/main/java/ru/dragomirov/taskschedule/auth/login/LoginController.java
+++ src/main/java/ru/dragomirov/taskschedule/auth/login/LoginController.java
@@ -1,16 +1,40 @@
 package ru.dragomirov.taskschedule.auth.login;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
 import ru.dragomirov.taskschedule.auth.User;
+import ru.dragomirov.taskschedule.commons.jwt.JwtTokenProvider;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/login")
+@RequiredArgsConstructor
 public class LoginController {
-    @GetMapping("/login")
-    public String get(
-            @ModelAttribute("user") User user
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> post(
+            @RequestBody User user
     ) {
-        return "auth/login";
+        UsernamePasswordAuthenticationToken authInputToken =
+                new UsernamePasswordAuthenticationToken(user.getUsername(),
+                        user.getPassword());
+
+        try {
+            authenticationManager.authenticate(authInputToken);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Incorrect credentials!"));
+        }
+
+        String token = jwtTokenProvider.generateToken(user.getUsername());
+        return ResponseEntity.ok(Map.of("jwt-token", token));
     }
 }
