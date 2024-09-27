f7324ffd1e1600481ce3715e4c213d9fe1db9eca
mesha <mikhail_dragomirov@mail.ru>
2024-09-27 17:53:38 +0300

    feat(1-005): обновил TaskDto

--- src/main/java/ru/dragomirov/taskschedule/core/task/TaskDto.java
+++ src/main/java/ru/dragomirov/taskschedule/core/task/TaskDto.java
@@ -14,24 +14,14 @@ public class TaskDto {
 
     @NotBlank(message = "Title cannot be blank")
     @Size(min = 1, max = 50, message = "Title must be between 3 and 50 characters")
-    @Pattern(regexp = "^[^\\s][\\S]*$", message = "Title cannot start with a space")
     public String title;
 
     @NotBlank(message = "Description cannot be blank")
     @Size(min = 1, max = 255, message = "Description must be between 3 and 50 characters")
-    @Pattern(regexp = "^[^\\s][\\S]*$", message = "Description cannot start with a space")
     public String description;
 
     @NotBlank(message = "Author cannot be blank")
     @Size(min = 1, max = 255, message = "Author must be between 3 and 50 characters")
-    @Pattern(regexp = "^[^\\s][\\S]*$", message = "Author cannot start with a space")
     public String author;
-
-    @NotBlank(message = "Status cannot be blank")
-    public Status status;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-    public LocalDateTime expirationData;
 
 }
