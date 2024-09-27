5e593a7ff09fb5e08481c8b6f12e4d3ed72d5dbc
mesha <mikhail_dragomirov@mail.ru>
2024-09-27 18:12:30 +0300

    feat(1-006): добавил class GetAllTaskController

---
+++ src/main/java/ru/dragomirov/taskschedule/core/task/get/GetAllTaskController.java
@@ -0,0 +1,27 @@
+package ru.dragomirov.taskschedule.core.task.get;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dragomirov.taskschedule.core.task.Task;
+import ru.dragomirov.taskschedule.core.task.TaskDto;
+import ru.dragomirov.taskschedule.core.task.TaskMapper;
+import ru.dragomirov.taskschedule.core.task.TaskService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks/getAll")
+@RequiredArgsConstructor
+public class GetAllTaskController {
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> get() {
+        List<Task> taskList = taskService.getAll();
+
+        return taskMapper.toDto(taskList);
+    }
+}
