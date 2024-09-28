acdf2784d4d14532cfc13d75134c314bb19ec5cc
mesha <mikhail_dragomirov@mail.ru>
2024-09-28 16:37:26 +0300

    feat(1-009): обновил для конкретного пользователя

--- src/main/java/ru/dragomirov/taskschedule/core/task/get/GetAllTaskController.java
+++ src/main/java/ru/dragomirov/taskschedule/core/task/get/GetAllTaskController.java
@@ -1,15 +1,19 @@
 package ru.dragomirov.taskschedule.core.task.get;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.dragomirov.taskschedule.auth.User;
+import ru.dragomirov.taskschedule.auth.UserService;
 import ru.dragomirov.taskschedule.core.task.Task;
 import ru.dragomirov.taskschedule.core.task.TaskDto;
 import ru.dragomirov.taskschedule.core.task.TaskMapper;
 import ru.dragomirov.taskschedule.core.task.TaskService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks/get-all")
@@ -17,10 +21,16 @@ import java.util.List;
 public class GetAllTaskController {
     private final TaskService taskService;
     private final TaskMapper taskMapper;
+    private final UserService userService;
 
     @GetMapping
-    public List<TaskDto> get() {
-        List<Task> taskList = taskService.getAll();
+    public List<TaskDto> get(
+            Authentication authentication
+    ) {
+        String userName = authentication.getName();
+        Optional<User> user = userService.getByUsername(userName);
+
+        List<Task> taskList = taskService.getAllByAuthorId(user.get().getId());
 
         return taskMapper.toDto(taskList);
     }
--- src/main/java/ru/dragomirov/taskschedule/core/task/get/GetStatusTaskController.java
+++ src/main/java/ru/dragomirov/taskschedule/core/task/get/GetStatusTaskController.java
@@ -1,10 +1,14 @@
 package ru.dragomirov.taskschedule.core.task.get;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import ru.dragomirov.taskschedule.auth.User;
+import ru.dragomirov.taskschedule.auth.UserService;
 import ru.dragomirov.taskschedule.core.task.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks/get-list-status")
@@ -12,12 +16,17 @@ import java.util.List;
 public class GetStatusTaskController {
     private final TaskService taskService;
     private final TaskMapper taskMapper;
+    private final UserService userService;
 
     @GetMapping("/{status}")
     public List<TaskDto> get(
-            @PathVariable Status status
+            @PathVariable Status status,
+            Authentication authentication
     ) {
-        List<Task> taskList = taskService.getByStatus(status);
+        String userName = authentication.getName();
+        Optional<User> user = userService.getByUsername(userName);
+
+        List<Task> taskList = taskService.getByStatus(status, user.get().getId());
 
         return taskMapper.toDto(taskList);
     }
