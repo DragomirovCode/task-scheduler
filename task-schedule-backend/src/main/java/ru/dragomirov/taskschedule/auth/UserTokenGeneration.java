8a2e7626778f4d64c233b1d33ee40fce4e079c45
mesha <mikhail_dragomirov@mail.ru>
2024-10-06 18:09:15 +0300

    feat(1-014.2): обновил логику для шаблона login.html

--- task-schedule-backend/src/main/java/ru/dragomirov/taskschedule/auth/UserTokenGeneration.java
+++ task-schedule-backend/src/main/java/ru/dragomirov/taskschedule/auth/UserTokenGeneration.java
@@ -3,7 +3,9 @@ package ru.dragomirov.taskschedule.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.dragomirov.taskschedule.commons.ResourceNotFoundException;
 import ru.dragomirov.taskschedule.commons.jwt.JwtTokenProvider;
 
 @Service
@@ -11,17 +13,27 @@ import ru.dragomirov.taskschedule.commons.jwt.JwtTokenProvider;
 public class UserTokenGeneration {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder; // Добавляем PasswordEncoder
     private final UserService userService;
 
     public String getToken(User user, String rawPassword) {
-        userService.getByPassword(rawPassword);
+        // Получаем пользователя по email
+        User currentUser = userService.getByEmail(user.getEmail());
 
+        // Сравниваем "сырой" пароль с хешированным
+        if (!passwordEncoder.matches(rawPassword, currentUser.getPassword())) {
+            throw new ResourceNotFoundException("User not found");
+        }
+
+        // Если пароли совпадают, аутентифицируем пользователя
         UsernamePasswordAuthenticationToken authInputToken =
-                new UsernamePasswordAuthenticationToken(user.getUsername(), rawPassword);
+                new UsernamePasswordAuthenticationToken(currentUser.getUsername(),
+                        rawPassword);
 
         authenticationManager.authenticate(authInputToken);
 
-        return jwtTokenProvider.generateToken(user.getUsername());
+        // Генерируем JWT-токен
+        return jwtTokenProvider.generateToken(currentUser.getUsername());
     }
 
     public String createToken(User user) {
