1076f29f132878dc3936637876fefa4468948228
mesha <mikhail_dragomirov@mail.ru>
2024-10-09 20:39:58 +0300

    feat(1-014.3.2): обновил логику для main.html шаблона

--- task-schedule-backend/src/main/java/ru/dragomirov/taskschedule/core/task/update/UpdateStatusTaskController.java
+++ task-schedule-backend/src/main/java/ru/dragomirov/taskschedule/core/task/update/UpdateStatusTaskController.java
@@ -2,10 +2,7 @@ package ru.dragomirov.taskschedule.core.task.update;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.dragomirov.taskschedule.auth.User;
 import ru.dragomirov.taskschedule.auth.UserService;
 import ru.dragomirov.taskschedule.core.task.Status;
@@ -23,6 +20,7 @@ public class UpdateStatusTaskController {
     @PatchMapping("/{id}")
     public UpdateTaskDto patch(
             @PathVariable Long id,
+            @RequestParam("direction") String direction,
             Authentication authentication
     ) {
         String currentUsername = authentication.getName();
@@ -30,10 +28,18 @@ public class UpdateStatusTaskController {
 
         Task task = taskService.getById(id);
 
-        if (task.getStatus() == Status.TODO) {
-            task.setStatus(Status.IN_PROGRESS);
-        } else if (task.getStatus() == Status.IN_PROGRESS) {
-            task.setStatus(Status.DONE);
+        if ("forward".equalsIgnoreCase(direction)) {
+            if (task.getStatus() == Status.TODO) {
+                task.setStatus(Status.IN_PROGRESS);
+            } else if (task.getStatus() == Status.IN_PROGRESS) {
+                task.setStatus(Status.DONE);
+            }
+        } else if ("backward".equalsIgnoreCase(direction)) {
+            if (task.getStatus() == Status.DONE) {
+                task.setStatus(Status.IN_PROGRESS);
+            } else if (task.getStatus() == Status.IN_PROGRESS) {
+                task.setStatus(Status.TODO);
+            }
         }
 
         taskService.update(id, task, user);
