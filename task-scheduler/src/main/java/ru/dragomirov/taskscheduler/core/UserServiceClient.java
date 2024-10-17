ccad3533b66e6849a562c92bc0b4239f88459cfc
mesha <mikhail_dragomirov@mail.ru>
2024-10-18 00:38:14 +0300

    feat(1-016.2): добавил UserServiceClient

---
+++ task-scheduler/src/main/java/ru/dragomirov/taskscheduler/core/UserServiceClient.java
@@ -0,0 +1,27 @@
+package ru.dragomirov.taskscheduler.core;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.dragomirov.taskschedulercommondto.kafka.UserDto;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceClient {
+    private final RestTemplate restTemplate;
+
+    public List<UserDto> getAllUsers() {
+        ResponseEntity<List<UserDto>> response = restTemplate.exchange(
+                "http://localhost:8080/api/user/get-all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<UserDto>>() {}
+        );
+        return response.getBody();
+    }
+}
