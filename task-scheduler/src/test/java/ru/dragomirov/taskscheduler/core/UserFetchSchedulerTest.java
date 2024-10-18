c6e374cade706eb60a175e81395bf6b3198949eb
mesha <mikhail_dragomirov@mail.ru>
2024-10-18 23:07:14 +0300

    feat(1-016.2): обновил

--- task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
+++ task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
@@ -14,7 +14,6 @@ import ru.dragomirov.taskschedulercommondto.kafka.UserDto;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.extension.ExtendWith;
