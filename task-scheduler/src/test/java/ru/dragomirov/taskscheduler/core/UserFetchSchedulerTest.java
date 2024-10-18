f3c2d08e479ab265e5a164588f88383cefdc222f
mesha <mikhail_dragomirov@mail.ru>
2024-10-19 01:05:24 +0300

    feat(1-016.2): обновил

--- task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
+++ task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
@@ -60,7 +60,7 @@ public class UserFetchSchedulerTest {
     }
 
     @Test
-    void getPendingTasksForDay_returnsUsersWithCompletedTasks() {
+    void getCompletedTasksForDay_returnsUsersWithCompletedTasks() {
         List<UserDto> users = Arrays.asList(
                 new UserDto(1L, "user1", "user1@example.com",
                         Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18"))),
@@ -81,5 +81,28 @@ public class UserFetchSchedulerTest {
         List<MessageDto> capturedMessages = captor.getAllValues();
         assertEquals(1, capturedMessages.size());
         assertEquals("DONE", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
+    }
+
+    @Test
+    void getAllTaskForDay_returnsUsersWithAllTasks() {
+        List<UserDto> users = Arrays.asList(
+                new UserDto(1L, "user1", "user1@example.com",
+                        Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18"))),
+                new UserDto(2L, "user2", "user2@example.com",
+                        Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19"))),
+                new UserDto(3L, "user3", "user3@example.com",
+                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20"),
+                                new TaskDto(3L, "Task4", "TODO", "2024-10-20")))
+        );
+
+        when(userServiceClient.getAllUsers()).thenReturn(users);
+
+        userFetchScheduler.getAllTaskForDay();
+
+        ArgumentCaptor<MessageDto> captor = ArgumentCaptor.forClass(MessageDto.class);
+        verify(emailProducer, times(3)).sendEmailMessage(captor.capture());
+
+        List<MessageDto> capturedMessages = captor.getAllValues();
+        assertEquals(3, capturedMessages.size());
     }
 }
