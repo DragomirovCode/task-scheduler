76660955123172157b3b189e3207a3a83b3d27ff
mesha <mikhail_dragomirov@mail.ru>
2024-10-18 22:12:51 +0300

    feat(1-016.2): обновил класс UserFetchScheduler

--- task-scheduler/src/main/java/ru/dragomirov/taskscheduler/core/UserFetchScheduler.java
+++ task-scheduler/src/main/java/ru/dragomirov/taskscheduler/core/UserFetchScheduler.java
@@ -18,19 +18,49 @@ public class UserFetchScheduler {
     private final EmailProducer emailProducer;
 
     @Scheduled(fixedRate = 30000)
-    public void checkAndSendEmailReminders() {
+    public void getPendingTasksForDay() {
         List<UserDto> users = userServiceClient.getAllUsers();
         String[] targetStatuses = {"TODO", "IN_PROGRESS"};
 
-        List<UserDto> usersWithIncompleteTasks = users.stream()
-                .filter(user -> user.getTaskDtos() != null)
-                .filter(user -> user.getTaskDtos().stream()
-                        .anyMatch(task -> List.of(targetStatuses).contains(task.getStatus())))
-                .collect(Collectors.toList());
+        List<UserDto> usersWithIncompleteTasks = sortUsersByStatus(users, targetStatuses);
 
         usersWithIncompleteTasks.forEach(user -> {
             MessageDto message = new MessageDto(user, "OUTSTANDING_TASKS", new Properties());
             emailProducer.sendEmailMessage(message);
         });
+    }
+
+    @Scheduled(fixedRate = 30000)
+    public void getCompletedTasksForDay() {
+        List<UserDto> users = userServiceClient.getAllUsers();
+        String[] targetStatuses = {"DONE"};
+
+        List<UserDto> usersWithTasks = sortUsersByStatus(users, targetStatuses);
+
+        usersWithTasks.forEach(user -> {
+            MessageDto message = new MessageDto(user, "COMPLETED_TASKS", new Properties());
+            emailProducer.sendEmailMessage(message);
+        });
+    }
+
+    @Scheduled(fixedRate = 30000)
+    public void getAllTaskForDay() {
+        List<UserDto> users = userServiceClient.getAllUsers();
+        String[] targetStatuses = {"TODO", "IN_PROGRESS", "DONE"};
+
+        List<UserDto> usersWithTasks = sortUsersByStatus(users, targetStatuses);
+
+        usersWithTasks.forEach(user -> {
+            MessageDto message = new MessageDto(user, "ALL_TASKS", new Properties());
+            emailProducer.sendEmailMessage(message);
+        });
+    }
+
+    private List<UserDto> sortUsersByStatus(List<UserDto> users, String[] targetStatuses) {
+        return users.stream()
+                .filter(user -> user.getTaskDtos() != null)
+                .filter(user -> user.getTaskDtos().stream()
+                        .anyMatch(task -> List.of(targetStatuses).contains(task.getStatus())))
+                .collect(Collectors.toList());
     }
 }
