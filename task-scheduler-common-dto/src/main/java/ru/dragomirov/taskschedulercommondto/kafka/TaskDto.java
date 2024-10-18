405eb58fea1c22b9c54b2350efa3e3cec786f99d
mesha <mikhail_dragomirov@mail.ru>
2024-10-19 01:19:26 +0300

    feat(1-016.2): обновил dto

--- task-scheduler-common-dto/src/main/java/ru/dragomirov/taskschedulercommondto/kafka/TaskDto.java
+++ task-scheduler-common-dto/src/main/java/ru/dragomirov/taskschedulercommondto/kafka/TaskDto.java
@@ -1,8 +1,12 @@
 package ru.dragomirov.taskschedulercommondto.kafka;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
 
     public Long id;
--- task-scheduler-common-dto/src/main/java/ru/dragomirov/taskschedulercommondto/kafka/UserDto.java
+++ task-scheduler-common-dto/src/main/java/ru/dragomirov/taskschedulercommondto/kafka/UserDto.java
@@ -1,10 +1,14 @@
 package ru.dragomirov.taskschedulercommondto.kafka;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     public Long id;
