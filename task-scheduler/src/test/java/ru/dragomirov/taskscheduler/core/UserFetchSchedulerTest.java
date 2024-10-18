f5a4b66306affa57f88b79fcf20b188c91a8dd6c
mesha <mikhail_dragomirov@mail.ru>
2024-10-18 23:33:55 +0300

    feat(1-016.2): обновил

--- task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
+++ task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
@@ -38,7 +38,8 @@ public class UserFetchSchedulerTest {
                 new UserDto(2L, "user2", "user2@example.com",
                         Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19"))),
                 new UserDto(3L, "user3", "user3@example.com",
-                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20")))
+                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20"),
+                                new TaskDto(3L, "Task4", "TODO", "2024-10-20")))
         );
 
         when(userServiceClient.getAllUsers()).thenReturn(users);
@@ -46,12 +47,14 @@ public class UserFetchSchedulerTest {
         userFetchScheduler.getPendingTasksForDay();
 
         ArgumentCaptor<MessageDto> captor = ArgumentCaptor.forClass(MessageDto.class);
-        verify(emailProducer, times(2)).sendEmailMessage(captor.capture());
+        verify(emailProducer, times(3)).sendEmailMessage(captor.capture());
 
         List<MessageDto> capturedMessages = captor.getAllValues();
-        assertEquals(2, capturedMessages.size());
+        assertEquals(3, capturedMessages.size());
         assertEquals("TODO", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
         assertEquals("IN_PROGRESS", capturedMessages.get(1).getUserDto().getTaskDtos().get(0).getStatus());
+        assertEquals("TODO", capturedMessages.get(2).getUserDto().getTaskDtos().get(0).getStatus());
+
         Assertions.assertNotEquals("DONE", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
         Assertions.assertNotEquals("DONE", capturedMessages.get(1).getUserDto().getTaskDtos().get(0).getStatus());
     }
