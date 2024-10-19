a50b7e3535cc9b5a468bc415cf7865d6352b28ef
mesha <mikhail_dragomirov@mail.ru>
2024-10-19 16:04:26 +0300

    feat(1-016.2): обновил. добавил больше тестов

--- task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
+++ task-scheduler/src/test/java/ru/dragomirov/taskscheduler/core/UserFetchSchedulerTest.java
@@ -11,6 +11,7 @@ import ru.dragomirov.taskschedulercommondto.kafka.MessageDto;
 import ru.dragomirov.taskschedulercommondto.kafka.TaskDto;
 import ru.dragomirov.taskschedulercommondto.kafka.UserDto;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,14 +33,18 @@ public class UserFetchSchedulerTest {
 
     @Test
     void getPendingTasksForDay_returnsUsersWithOutstandingTasks() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        LocalDateTime weekAgo = LocalDateTime.now().minusDays(7);
+
         List<UserDto> users = Arrays.asList(
                 new UserDto(1L, "user1", "user1@example.com",
-                        Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18"))),
+                        Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18", today))),
                 new UserDto(2L, "user2", "user2@example.com",
-                        Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19"))),
+                        Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19", yesterday))),
                 new UserDto(3L, "user3", "user3@example.com",
-                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20"),
-                                new TaskDto(3L, "Task4", "TODO", "2024-10-20")))
+                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20", yesterday),
+                                new TaskDto(4L, "Task4", "TODO", "2024-10-20", weekAgo)))
         );
 
         when(userServiceClient.getAllUsers()).thenReturn(users);
@@ -47,28 +52,29 @@ public class UserFetchSchedulerTest {
         userFetchScheduler.getPendingTasksForDay();
 
         ArgumentCaptor<MessageDto> captor = ArgumentCaptor.forClass(MessageDto.class);
-        verify(emailProducer, times(3)).sendEmailMessage(captor.capture());
+        verify(emailProducer, times(1)).sendEmailMessage(captor.capture());
 
         List<MessageDto> capturedMessages = captor.getAllValues();
-        assertEquals(3, capturedMessages.size());
-        assertEquals("TODO", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
-        assertEquals("IN_PROGRESS", capturedMessages.get(1).getUserDto().getTaskDtos().get(0).getStatus());
-        assertEquals("TODO", capturedMessages.get(2).getUserDto().getTaskDtos().get(0).getStatus());
+        assertEquals(1, capturedMessages.size());
+        assertEquals("IN_PROGRESS", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
 
         Assertions.assertNotEquals("DONE", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
-        Assertions.assertNotEquals("DONE", capturedMessages.get(1).getUserDto().getTaskDtos().get(0).getStatus());
     }
 
     @Test
     void getCompletedTasksForDay_returnsUsersWithCompletedTasks() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        LocalDateTime weekAgo = LocalDateTime.now().minusDays(7);
+
         List<UserDto> users = Arrays.asList(
                 new UserDto(1L, "user1", "user1@example.com",
-                        Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18"))),
+                        Arrays.asList(new TaskDto(1L, "Task1", "DONE", "2024-10-18", today))),
                 new UserDto(2L, "user2", "user2@example.com",
-                        Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19"))),
+                        Arrays.asList(new TaskDto(2L, "Task2", "DONE", "2024-10-19", yesterday))),
                 new UserDto(3L, "user3", "user3@example.com",
-                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20"),
-                                new TaskDto(3L, "Task4", "TODO", "2024-10-20")))
+                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20", weekAgo),
+                                new TaskDto(4L, "Task4", "DONE", "2024-10-20", yesterday)))
         );
 
         when(userServiceClient.getAllUsers()).thenReturn(users);
@@ -76,23 +82,28 @@ public class UserFetchSchedulerTest {
         userFetchScheduler.getCompletedTasksForDay();
 
         ArgumentCaptor<MessageDto> captor = ArgumentCaptor.forClass(MessageDto.class);
-        verify(emailProducer, times(1)).sendEmailMessage(captor.capture());
+        verify(emailProducer, times(2)).sendEmailMessage(captor.capture());
 
         List<MessageDto> capturedMessages = captor.getAllValues();
-        assertEquals(1, capturedMessages.size());
+        assertEquals(2, capturedMessages.size());
         assertEquals("DONE", capturedMessages.get(0).getUserDto().getTaskDtos().get(0).getStatus());
+        assertEquals("DONE", capturedMessages.get(1).getUserDto().getTaskDtos().get(0).getStatus());
     }
 
     @Test
     void getAllTaskForDay_returnsUsersWithAllTasks() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        LocalDateTime weekAgo = LocalDateTime.now().minusDays(7);
+
         List<UserDto> users = Arrays.asList(
                 new UserDto(1L, "user1", "user1@example.com",
-                        Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18"))),
+                        Arrays.asList(new TaskDto(1L, "Task1", "TODO", "2024-10-18", today))),
                 new UserDto(2L, "user2", "user2@example.com",
-                        Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19"))),
+                        Arrays.asList(new TaskDto(2L, "Task2", "IN_PROGRESS", "2024-10-19", yesterday))),
                 new UserDto(3L, "user3", "user3@example.com",
-                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20"),
-                                new TaskDto(4L, "Task4", "TODO", "2024-10-20")))
+                        Arrays.asList(new TaskDto(3L, "Task3", "DONE", "2024-10-20", yesterday),
+                                new TaskDto(4L, "Task4", "TODO", "2024-10-20", yesterday)))
         );
 
         when(userServiceClient.getAllUsers()).thenReturn(users);
@@ -105,6 +116,6 @@ public class UserFetchSchedulerTest {
         List<MessageDto> capturedMessages = captor.getAllValues();
 
         assertEquals(1, capturedMessages.size());
-        assertEquals("user3", capturedMessages.get(0).userDto.username);
+        assertEquals("user3", capturedMessages.get(0).getUserDto().getUsername());
     }
 }
